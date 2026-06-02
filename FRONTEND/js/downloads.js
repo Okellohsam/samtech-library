ae93a0b1bd1d0df63f96083e17ffd57780fdef7b
bluckwolf <114413906+Okellohsam@users.noreply.github.com>
2026-06-02 13:03:30 +0300

    Update downloads.js

--- FRONTEND/js/downloads.js
+++ FRONTEND/js/downloads.js
@@ -1,7 +1,7 @@
 const downloadPDF = async (id) => {
 
   const response = await fetch(
-    `http://localhost:5000/api/pdfs/download/${id}`
+    `https://samtech-library.onrender.com/api/pdfs/download/${id}`
   );
 
   const data = await response.json();

df7ab47bd778207fc147d322910dcf4700009047
bluckwolf <114413906+Okellohsam@users.noreply.github.com>
2026-06-05 11:05:02 +0300

    Add files via upload

---
+++ FRONTEND/js/admin.js
@@ -0,0 +1,46 @@
+const loginForm =
+  document.getElementById('loginForm');
+
+loginForm?.addEventListener(
+  'submit',
+  async (e) => {
+
+    e.preventDefault();
+
+    const username =
+      document.getElementById('username').value;
+
+    const password =
+      document.getElementById('password').value;
+
+    const response = await fetch(
+      'https://samtech-library.onrender.com/api/admin/login',
+      {
+        method: 'POST',
+
+        headers: {
+          'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+          username,
+          password
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    if (data.token) {
+
+      localStorage.setItem(
+        'token',
+        data.token
+      );
+
+      window.location.href =
+        '../admin/dashboard.html';
+    }
+
+  }
+);
